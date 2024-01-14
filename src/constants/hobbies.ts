389cf7cc973983b7256bb61a4d431f02d839809e
MatchaTi <tehmatcha28@gmail.com>
2024-01-14 19:53:57 +0800

    feat : add constans hobbies & navLists

---
+++ src/constants/hobbies.ts
@@ -0,0 +1,5 @@
+export const hobbies = [
+  { name: "Playing Rhythm Games", icon: "simple-icons:osu" },
+  { name: "Learning Guitar", icon: "la:guitar" },
+  { name: "Playing with My Cats", icon: "solar:cat-outline" },
+];
---
+++ src/constants/navLists.ts
@@ -0,0 +1,22 @@
+export const navLists = [
+  {
+    name: 'home',
+    path: '/',
+    icon: 'bx:home',
+  },
+  {
+    name: 'stack',
+    path: '/stack',
+    icon: 'charm:stack'
+  },
+  {
+    name: 'projects',
+    path: '/projects',
+    icon: 'mingcute:grid-2-line',
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: 'fluent:clover-16-regular',
+  },
+];
