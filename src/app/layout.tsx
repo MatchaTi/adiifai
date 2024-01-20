02dfa78d5e40ea4c755a6ba43b604a6fce49e3eb
MatchaTi <tehmatcha28@gmail.com>
2024-01-20 13:20:46 +0800

    chore : update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
             alt="Blue Wallpaper"
           />
           <div className="w-full max-w-5xl mx-auto px-4 lg:flex lg:gap-16 lg:mt-8">
-            <div className="pt-4 bg-light-bg sticky top-0 lg:top-16 lg:h-full lg:max-w-[200px] lg:w-full lg:pt-0 dark:bg-dark-bg">
+            <div className="pt-4 bg-light-bg sticky top-0 z-50 lg:top-16 lg:h-full lg:max-w-[200px] lg:w-full lg:pt-0 dark:bg-dark-bg">
               <Navbar />
             </div>
             <div className="lg:-mt-32 w-full">
